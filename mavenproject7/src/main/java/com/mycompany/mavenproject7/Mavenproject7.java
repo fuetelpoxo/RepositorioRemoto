1adb17eb7b4519cca9f98269114173a9b1675616
fuetelpoxo <paratodolodeangel@gmail.com>
2024-06-21 16:00:30 +0200

    cambio

---
+++ mavenproject7/src/main/java/com/mycompany/mavenproject7/Mavenproject7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject7;
+
+/**
+ *
+ * @author DAW106
+ */
+public class Mavenproject7 {
+
+    public static void main(String[] args) {
+        System.out.println("afaf");
+    }
+}
